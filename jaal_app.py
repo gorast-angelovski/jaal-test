16e049518becae586d069249ced55d8672c70f18
Gorast Angelovski <gorast.angelovski1@gmail.com>
2022-05-26 13:01:09 +0200

    Started gunicorn server

--- jaal_app.py
+++ jaal_app.py
@@ -4,4 +4,6 @@ from jaal.datasets import load_got
 # load the data
 edge_df, node_df = load_got()
 # init Jaal and run server
-Jaal(edge_df, node_df).plot()
+app = Jaal(edge_df, node_df).create()
+server = app.server
+# Jaal(edge_df, node_df).plot()
